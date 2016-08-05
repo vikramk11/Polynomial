b5cf970b3c5a2fc66b52a677da65f3df7490156f
Rajat Grewal <rajatgrewal1@gmail.com>
2016-08-05 09:00:29 +0530

    Add files via upload

---
+++ Polynomial.java
@@ -0,0 +1,89 @@
+public class Polynomial {
+    private int[] coef;
+    private int deg;     
+
+  
+    public Polynomial(int a, int b) {
+        coef = new int[b+1];
+        coef[b] = a;
+        deg = degree();
+    }
+
+    public int degree() {
+        int d = 0;
+        for (int i = 0; i < coef.length; i++)
+            if (coef[i] != 0) d = i;
+        return d;
+    }
+
+  
+    public Polynomial addition(Polynomial b) {
+        Polynomial a = this;
+        Polynomial c = new Polynomial(0, Math.max(a.deg, b.deg));
+        for (int i = 0; i <= a.deg; i++) c.coef[i] += a.coef[i];
+        for (int i = 0; i <= b.deg; i++) c.coef[i] += b.coef[i];
+        c.deg = c.degree();
+        return c;
+    }
+
+    public Polynomial subtraction(Polynomial b) {
+        Polynomial a = this;
+        Polynomial c = new Polynomial(0, Math.max(a.deg, b.deg));
+        for (int i = 0; i <= a.deg; i++) c.coef[i] += a.coef[i];
+        for (int i = 0; i <= b.deg; i++) c.coef[i] -= b.coef[i];
+        c.deg = c.degree();
+        return c;
+    }
+
+    public Polynomial multiplication(Polynomial b) {
+        Polynomial a = this;
+        Polynomial c = new Polynomial(0, a.deg + b.deg);
+        for (int i = 0; i <= a.deg; i++)
+            for (int j = 0; j <= b.deg; j++)
+                c.coef[i+j] += (a.coef[i] * b.coef[j]);
+        c.deg = c.degree();
+        return c;
+    }
+ 
+    public String toString() {
+        if (deg ==  0) return "" + coef[0];
+        if (deg ==  1) return coef[1] + "x + " + coef[0];
+        String s = coef[deg] + "x^" + deg;
+        for (int i = deg-1; i >= 0; i--) {
+            if      (coef[i] == 0) continue;
+            else if (coef[i]  > 0) s = s + " + " + ( coef[i]);
+            else if (coef[i]  < 0) s = s + " - " + (-coef[i]);
+            if      (i == 1) s = s + "x";
+            else if (i >  1) s = s + "x^" + i;
+        }
+        return s;
+    }
+
+   
+    public static void main(String[] args) { 
+        Polynomial zero = new Polynomial(0, 0);
+
+        Polynomial p1   = new Polynomial(4, 3);
+        Polynomial p2   = new Polynomial(3, 2);
+        Polynomial p3   = new Polynomial(1, 0);
+        Polynomial p4   = new Polynomial(2, 1);
+        Polynomial p    = p1.addition(p2).addition(p3).addition(p4);  
+
+        Polynomial q1   = new Polynomial(3, 2);
+        Polynomial q2   = new Polynomial(5, 0);
+        Polynomial q    = q1.addition(q2);                     
+
+
+        Polynomial r    = p.addition(q);
+        Polynomial s    = p.multiplication(q);
+
+        System.out.println("zero(x) =     " + zero);
+        System.out.println("p(x) =        " + p);
+        System.out.println("q(x) =        " + q);
+        System.out.println("p(x) + q(x) = " + r);
+        System.out.println("p(x) * q(x) = " + s);
+        System.out.println("0 - p(x)    = " + zero.subtraction(p));
+
+   }
+
+}
